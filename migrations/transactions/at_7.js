f312dd7cf1931c7136136a133e06c9730d62d8f4
nionis <stivennoni77@gmail.com>
2018-01-11 23:04:01 +0200

    migration transactions for hodlFor

---
+++ migrations/transactions/at_7.js
@@ -0,0 +1,16 @@
+const Interact = require("../../utils/Interact")
+const Keys = require("../../keys");
+const [, , networkName] = process.argv;
+const interact = Interact(networkName);
+const { Eth, eth, contracts, provider } = interact;
+
+(async () => {
+  const keys = Keys(networkName);
+  const { Eth, eth, contracts, provider } = interact;
+  const { HodlFor } = contracts;
+  const deployer = provider.address;
+  const ops = { from: deployer }
+
+  // transfer ownership to athene
+  await HodlFor.transferOwnership(keys.athene, ops).then(console.log).catch(console.log)
+})();
